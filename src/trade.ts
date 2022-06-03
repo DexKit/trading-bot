8a9a73a5c0edf98a9306308324b0340c409c30da
JoaoCampos89 <jcampos893@gmail.com>
2022-06-03 12:57:08 -0300

    add missing slippage params

--- src/trade.ts
+++ src/trade.ts
@@ -19,6 +19,8 @@ export const executeTrade = async (market: MarketData) => {
 
             const sellToken = market.quoteTokenBalance.token.address;
             const buyToken = market.baseTokenBalance.token.address;
+            const slippagePercentage = market.slippagePercentage;
+
             let randomBuyAmount = market.minBuyUnit + market.maxBuyUnit * Math.random();
             if (randomBuyAmount > market.maxBuyUnit) {
                 randomBuyAmount = market.maxBuyUnit;
@@ -28,7 +30,7 @@ export const executeTrade = async (market: MarketData) => {
             }
             const sellAmount = parseUnits(randomBuyAmount.toFixed(6), market.quoteTokenBalance.token.decimals).toString();
             if (Number(randomBuyAmount) > market.minBuyUnit) {
-                const quote = await getSwapQuote({ sellAmount, sellToken, buyToken, takerAddress, intentOnFill: true }, market.chainId);
+                const quote = await getSwapQuote({ sellAmount, sellToken, buyToken, takerAddress, intentOnFill: true, slippagePercentage }, market.chainId);
                 console.log(`bot doing a buy worth of ${randomBuyAmount}`);
                 if (!IS_SIMULATION) {
                     const { data, to, value, gas, gasPrice } = quote.data;
@@ -49,12 +51,13 @@ export const executeTrade = async (market: MarketData) => {
         try {
             const sellToken = market.baseTokenBalance.token.address;
             const buyToken = market.quoteTokenBalance.token.address;
+            const slippagePercentage = market.slippagePercentage;
             let randomSellAmount = market.minSellUnit + market.maxSellUnit * Math.random();
             if (randomSellAmount > market.maxSellUnit) {
                 randomSellAmount = market.maxSellUnit;
             }
             const buyAmount = parseUnits(randomSellAmount.toFixed(6), market.quoteTokenBalance.token.decimals).toString();
-            const quote = await getSwapQuote({ buyAmount, sellToken, buyToken, takerAddress, intentOnFill: false }, market.chainId);
+            const quote = await getSwapQuote({ buyAmount, sellToken, buyToken, takerAddress, intentOnFill: false, slippagePercentage }, market.chainId);
 
 
 
@@ -62,7 +65,7 @@ export const executeTrade = async (market: MarketData) => {
                 //const sellAmount = market.baseTokenBalance.balance.toString();
 
                 console.log(`bot doing a sell worth ${randomSellAmount}`);
-                const quote = await getSwapQuote({ buyAmount, sellToken, buyToken, takerAddress, intentOnFill: true }, market.chainId);
+                const quote = await getSwapQuote({ buyAmount, sellToken, buyToken, takerAddress, intentOnFill: true, slippagePercentage }, market.chainId);
                 if (!IS_SIMULATION) {
                     const { data, to, value, gas, gasPrice } = quote.data;
                     const tx = await signer.sendTransaction({ data, to, value, gasLimit: gas, gasPrice });
