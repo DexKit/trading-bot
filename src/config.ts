5c1a21d2d9ceb1ec47ebab803689b223ea5f5457
JoaoCampos89 <jcampos893@gmail.com>
2022-07-14 09:39:55 -0300

    update bot params

--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,10 @@ export const Markets: MarketConfig[] = [
         baseTokenAddress: '0x4D0Def42Cf57D6f27CD4983042a55dce1C9F853c',
         quoteTokenAddress: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
         chainId: ChainId.Polygon,
-        maxBuyUnit: 8,
+        maxBuyUnit: 15,
         minBuyUnit: 3,
         minSellUnit: 3,
-        maxSellUnit: 8,
+        maxSellUnit: 15,
         minIntervalSeconds: 60 * 10,
         maxIntervalSeconds: 60 * 20,
     }
