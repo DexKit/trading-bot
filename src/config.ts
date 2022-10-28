4d9e91a6dbfce8c17630d72fa23882a72e37ce83
JoaoCampos89 <jcampos893@gmail.com>
2022-10-28 11:11:05 -0300

    fine tune bot

--- src/config.ts
+++ src/config.ts
@@ -6,11 +6,11 @@ export const Markets: MarketConfig[] = [
         baseTokenAddress: '0x4D0Def42Cf57D6f27CD4983042a55dce1C9F853c',
         quoteTokenAddress: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
         chainId: ChainId.Polygon,
-        maxBuyUnit: 15,
-        minBuyUnit: 3,
-        minSellUnit: 3,
-        maxSellUnit: 15,
-        minIntervalSeconds: 60 * 10,
-        maxIntervalSeconds: 60 * 20,
+        maxBuyUnit: 25,
+        minBuyUnit: 5,
+        minSellUnit: 5,
+        maxSellUnit: 25,
+        minIntervalSeconds: 60 * 5,
+        maxIntervalSeconds: 60 * 10,
     }
 ]
